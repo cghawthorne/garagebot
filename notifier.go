0e8748eeb9f3df8c13cc981a7a119e4a19adca98
Curtis "Fjord" Hawthorne <cghawthorne@gmail.com>
2025-09-27 13:25:12 -0700

    Remove redundant openAlertSent reset

--- notifier.go
+++ notifier.go
@@ -14,10 +14,29 @@ const (
 	NOTIFICATION_CHECK_PERIOD = time.Duration(1) * time.Second
 )
 
+func sendNotificationEmail(config *Configuration, status string) {
+	to := strings.Join(config.Notifications.Emails, ",")
+	msg := "To: " + to + "\r\n" +
+		"Subject: " + status + "\r\n" +
+		"\r\n" +
+		status
+
+	log.Print("Sending email: ", msg)
+
+	auth := smtp.PlainAuth("",
+		config.Notifications.From, config.Notifications.Password, config.Notifications.Server)
+	err := smtp.SendMail(config.Notifications.Server+":"+strconv.Itoa(config.Notifications.Port), auth,
+		config.Notifications.From, config.Notifications.Emails, []byte(msg))
+
+	if err != nil {
+		log.Printf("smtp error: %v", err)
+	}
+}
+
 func notifier(config *Configuration, statusUpdates StatusUpdateChan) {
 	statusChange := time.Now()
 	lastStatus := STARTUP
-	notificationSent := false
+	openAlertSent := false
 
 	timeout := time.Duration(config.Notifications.TimeoutMillis) * time.Millisecond
 
@@ -25,33 +44,21 @@ func notifier(config *Configuration, statusUpdates StatusUpdateChan) {
 	for {
 		select {
 		case <-ticker.C:
-			if !notificationSent && lastStatus == NOTIFICATION_STATUS && time.Since(statusChange) > timeout {
-				notificationSent = true
-
-				to := strings.Join(config.Notifications.Emails, ",")
+			if !openAlertSent && lastStatus == NOTIFICATION_STATUS && time.Since(statusChange) > timeout {
 				status := fmt.Sprintf("Door has been %v for %v", lastStatus.String(), timeout)
-
-				msg := "To: " + to + "\r\n" +
-					"Subject: " + status + "\r\n" +
-					"\r\n" +
-					status
-
-				log.Print("Sending email: ", msg)
-
-				auth := smtp.PlainAuth("",
-					config.Notifications.From, config.Notifications.Password, config.Notifications.Server)
-				err := smtp.SendMail(config.Notifications.Server+":"+strconv.Itoa(config.Notifications.Port), auth,
-					config.Notifications.From, config.Notifications.Emails, []byte(msg))
-
-				if err != nil {
-					log.Printf("smtp error: %v", err)
-				}
+				sendNotificationEmail(config, status)
+				openAlertSent = true
 			}
 		case update := <-statusUpdates:
 			log.Print("Notifier got update:", update)
+			if update == CLOSED && openAlertSent {
+				sendNotificationEmail(config, "Door has closed")
+			}
+			if update != NOTIFICATION_STATUS {
+				openAlertSent = false
+			}
 			lastStatus = update
 			statusChange = time.Now()
-			notificationSent = false
 		}
 	}
 }
